a12db2da972a5150b885161af33b7f37d0352a43
saman <saman.k8121@gmail.com>
2024-12-25 12:07:57 +0300

    chore: refactor k6 test

--- test/k6/account.ts
+++ test/k6/account.ts
@@ -7,32 +7,32 @@ export const options = {
   // vus: 1,
   // duration: '30s',
   //load test
-  stages:[ 
-    {duration:'5s', target:'200'},
-    {duration:'2m', target:'200'},
-    {duration:'5s', target:'0'}
+  stages: [
+    { duration: '5s', target: '200' },
+    { duration: '2m', target: '200' },
+    { duration: '5s', target: '0' },
   ],
   //stress test
-  // stages:[ 
-  //   {duration:'1m', target:'200'}, //ramp up
-  //   {duration:'5m', target:'200'}, //stable
-  //   {duration:'1m', target:'400'},//ramp up
-  //   {duration:'5m', target:'400'},//stable
-  //   {duration:'1m', target:'800'},//ramp up
-  //   {duration:'5m', target:'800'},//stable
-  //   {duration:'1m', target:'0'}
+  // stages: [
+  //   { duration: '1m', target: '200' }, //ramp up
+  //   { duration: '5m', target: '200' }, //stable
+  //   { duration: '1m', target: '400' }, //ramp up
+  //   { duration: '5m', target: '400' }, //stable
+  //   { duration: '1m', target: '800' }, //ramp up
+  //   { duration: '5m', target: '800' }, //stable
+  //   { duration: '1m', target: '0' },
   // ],
   //spike test
-  // stages:[ 
-  //   {duration:'30s', target:'2000'}, //ramp up
-  //   {duration:'5m', target:'2000'}, //stable
-  //   {duration:'30s', target:'0'}
+  // stages: [
+  //   { duration: '30s', target: '2000' }, //ramp up
+  //   { duration: '5m', target: '2000' }, //stable
+  //   { duration: '30s', target: '0' },
   // ],
   //soak test
-  // stages:[ 
-  //   {duration:'2m', target:'200'}, //ramp up
-  //   {duration:'8h', target:'200'}, //stable
-  //   {duration:'2m', target:'0'}
+  // stages: [
+  //   { duration: '2m', target: '200' }, //ramp up
+  //   { duration: '8h', target: '200' }, //stable
+  //   { duration: '2m', target: '0' },
   // ],
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
@@ -66,7 +66,10 @@ export default function (data) {
       Authorization: `Bearer ${data.token}`,
     },
   };
-  http.get('http://localhost:3000/api/account/cm4sbbl0g0000veckjmf2nyw8', headers);
+  http.get(
+    'http://localhost:3000/api/account/cm4sbbl0g0000veckjmf2nyw8',
+    headers
+  );
   //http.get(`http://localhost:3000/api/account/${randomSlug}`);
   sleep(1);
 }
